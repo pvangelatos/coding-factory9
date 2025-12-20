d01007f2ba7a995ad99db32b76d6fffb1bd81b8a
pvangelatos <p.vangelatos23@hotmail.com>
2025-12-20 11:46:40 +0200

    feat: add hashset logic with api

---
+++ src/gr/aueb/cf/cf9/ch17/sets/Main.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.cf9.ch17.sets;
+
+import java.sql.SQLOutput;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<String> bag = new HashSet<>();
+
+        bag.add("Apple");
+        bag.add("Banana");
+        bag.add("Orange");
+        bag.add("Apple"); // Duplicate, will not be added
+        System.out.println("Bag contents: " + bag);
+
+        if (bag.contains("Apple")) {    // overide equals
+            bag.remove("Apple");
+        } else {
+            System.out.println("Apple not found in the bag.");
+        }
+        bag.forEach(System.out::println);
+    }
+}
