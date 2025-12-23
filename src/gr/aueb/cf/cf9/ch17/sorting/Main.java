6de50c2bc73abc966c171589ddbf66fb3c7f4eca
pvangelatos <p.vangelatos23@hotmail.com>
2025-12-23 17:08:08 +0200

    feat: add sorting with Comparator interface

---
+++ src/gr/aueb/cf/cf9/ch17/sorting/Main.java
@@ -0,0 +1,30 @@
+package gr.aueb.cf.cf9.ch17.sorting;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("Apples", 12.5, 10),
+                new Product("Oranges", 8.1, 50),
+                new Product("Milk", 2.8, 4),
+                new Product("Apples", 22.5, 120)
+        ));
+
+        products.sort(Comparator.naturalOrder());  // ascending
+        products.forEach(System.out::println);
+
+        products.sort(Comparator.reverseOrder());  // descending
+
+        products.sort(Comparator.comparing(Product::getPrice));
+        //  products.sort((a, b) -> a.getQuantity() - b.getQuantity());     // ascending order
+
+        products.sort(Comparator.comparing(Product::getQuantity)
+                .thenComparing(Product::getPrice, Comparator.reverseOrder())    // price descending
+                .thenComparing(Product::getDescription).reversed());
+
+    }
+}
