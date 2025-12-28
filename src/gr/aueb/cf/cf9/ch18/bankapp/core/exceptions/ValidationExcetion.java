437edbd24ea3e7605bfeca5f838be14feb82376c
pvangelatos <p.vangelatos23@hotmail.com>
2025-12-28 13:08:53 +0200

    feat (Exception): add exceptions definitions

---
+++ src/gr/aueb/cf/cf9/ch18/bankapp/core/exceptions/ValidationExcetion.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.cf9.ch18.bankapp.core.exceptions;
+
+public class ValidationExcetion extends Exception{
+
+    public ValidationExcetion(String message) {
+        super(message);
+    }
+}
