e3fed5094c6b36454c46b8e79d8b43ac56740a69
pvangelatos <p.vangelatos23@hotmail.com>
2026-01-09 16:58:44 +0200

    fix: Account code

--- src/gr/aueb/cf/cf9/ch18/bankapp/model/Account.java
+++ src/gr/aueb/cf/cf9/ch18/bankapp/model/Account.java
@@ -15,21 +15,13 @@ public class Account {
         this.balance = balance;
     }
 
-    public String getIban() {
-        return iban;
-    }
+    public String getIban() { return iban; }
 
-    public void setIban(String iban) {
-        this.iban = iban;
-    }
+    public void setIban(String iban) { this.iban = iban; }
 
-    public double getBalance() {
-        return balance;
-    }
+    public double getBalance() { return balance; }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+    public void setBalance(double balance) { this.balance = balance; }
 
     @Override
     public String toString() {
