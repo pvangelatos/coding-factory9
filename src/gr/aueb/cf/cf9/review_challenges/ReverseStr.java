ba10addcef78a08bfccdbc68655d40ea2bcbefab
pvangelatos <p.vangelatos23@hotmail.com>
2026-01-12 11:42:18 +0200

    feat: add ReverseStringApp

---
+++ src/gr/aueb/cf/cf9/review_challenges/ReverseStr.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.cf9.review_challenges;
+
+public class ReverseStr {
+
+    public static String reverse(String str) {
+
+        return new StringBuilder(str).reverse().toString();
+    }
+}
