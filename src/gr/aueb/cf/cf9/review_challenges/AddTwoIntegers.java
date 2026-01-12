e46315abe9b7357300ff95e5ed707cc804618567
pvangelatos <p.vangelatos23@hotmail.com>
2026-01-12 11:30:14 +0200

    fix: naming convention

--- src/gr/aueb/cf/cf9/review_challenges/AddTwoIntegers.java
+++ src/gr/aueb/cf/cf9/review_challenges/AddTwoIntegers.java
@@ -2,7 +2,7 @@ package gr.aueb.cf.cf9.review_challenges;
 
 public class AddTwoIntegers {
 
-    public static int Sum (int a, int b) {
+    public static int sum (int a, int b) {
         return a + b;
     }
 }
