491482315f036b2022fed6a620da75e7cce6618e
pvangelatos <p.vangelatos23@hotmail.com>
2026-01-13 15:37:51 +0200

    fix: override toString

--- src/gr/aueb/cf/cf9/ch18/bankapp/dto/AccountReadOnlyDTO.java
+++ src/gr/aueb/cf/cf9/ch18/bankapp/dto/AccountReadOnlyDTO.java
@@ -3,4 +3,9 @@ package gr.aueb.cf.cf9.ch18.bankapp.dto;
 import java.math.BigDecimal;
 
 public record AccountReadOnlyDTO(String iban, BigDecimal balance) {
+
+    @Override
+    public String toString() {
+        return "IBAN: " + iban + ", Balance: " + balance;
+    }
 }
