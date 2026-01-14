20b8c0d42dc8e85a66a70c9814144543076874ed
pvangelatos <p.vangelatos23@hotmail.com>
2026-01-14 17:05:50 +0200

    feat: add non-capturing groups

---
+++ src/gr/aueb/cf/cf9/ch19/NonCapturing.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.cf9.ch19;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Recognize but not capture
+ */
+public class NonCapturing {
+
+    public static void main(String[] args) {
+        String s = "HelloWorld HelloWorld";
+
+        Pattern pattern = Pattern.compile("(Hello)(?:World)");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+            for (int i = 1; i <= matcher.groupCount() ; i++) {
+                System.out.print(matcher.group(i) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
